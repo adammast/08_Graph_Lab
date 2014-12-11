7b9e6b9816af5af198f2f91f68ce5868605713fd
Adam Powell <adamm2powell@gmail.com>
2014-12-10 19:19:59 -0500

    Finished code

--- Graph.cpp
+++ Graph.cpp
@@ -6,22 +6,71 @@
 #include "Graph.h"
 
 Graph::Graph(unsigned int numNodes){
-  //TODO
+	adjList.resize(numNodes);
 }
 
 double Graph::getCost(int node1, int node2){
-  //TODO
-  return -1.0;
+	if (!(node1 >= adjList.size())){
+		for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
+			if (adjList[node1].edgeList[i].dest == node2) {
+				return adjList[node1].edgeList[i].cost;
+			}
+		}
+	}
+	return -1.0;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+	if (cost < 0){
+		throw std::string("Error: The cost cannot be negative");
+	}
+	if (node1 < 0 || node1 > adjList.size()){
+		throw std::string("Error: Node 1 was outside range");
+	}
+	if (node2 < 0 || node1 > adjList.size()){
+		throw std::string("Error: Node 2 was outside range");
+	}
+
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++) {
+		if (adjList[node1].edgeList[i].dest == node2){
+			adjList[node1].edgeList[i].cost = cost;
+			
+				for (unsigned int j = 0; j < adjList[node2].edgeList.size(); j++) {
+					if (adjList[node2].edgeList[j].dest == node1){
+						adjList[node2].edgeList[j].cost = cost;
+						return;
+					}
+				}
+		}
+	}
+
+	adjList[node1].edgeList.push_back(Edge(cost, node2));
+	adjList[node2].edgeList.push_back(Edge(cost, node1));
+
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+	if (node1 < 0 || node1 > adjList.size()){
+		throw std::string("Node 1 was outisde of range");
+	}
+	if (node2 < 0 || node1 > adjList.size()){
+		throw std::string("Node 2 was outside of range");
+	}
+
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++) {
+		if (adjList[node1].edgeList[i].dest == node2){
+			adjList[node1].edgeList.erase(adjList[node1].edgeList.begin() + i);
+
+			for (unsigned int j = 0; j < adjList[node2].edgeList.size(); j++) {
+				if (adjList[node2].edgeList[j].dest == node1){
+					adjList[node2].edgeList.erase(adjList[node2].edgeList.begin() + j);
+					return;
+				}
+			}
+		}
+	}
 }
